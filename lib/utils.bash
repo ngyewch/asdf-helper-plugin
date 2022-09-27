37330720392fb6e65e0e0209b53259ea7f7d2d59
Nick Ng <ngyewch@gmail.com>
2022-09-27 11:12:41 +0800

    fix: download script

--- lib/utils.bash
+++ lib/utils.bash
@@ -45,6 +45,7 @@ download_release() {
   url="$GH_REPO/archive/v${version}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
+  echo "${url}"
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
